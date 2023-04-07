d2052afb6f732faf36b076aea416e6b3c4dd7d77
Siyabonga-sudo <siyabongamsipa00@gmail.com>
2023-04-07 19:08:09 +0200

    Update API for current weather

--- src/weather.js
+++ src/weather.js
@@ -33,15 +33,15 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
 
-  temperatureElement.innerHTML = Math.round(response.temperature.current);
-  cityElement.innerHTML = response.data[0].city;
-  descriptionElement.innerHTML = response.condition;
-  humidityElement.innerHTML = response.temperature.humidity;
-  windElement.innerHTML = Math.round(response.wind.speed);
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = `e9tb1630o25a4f01cebc66c9ef1df31c`;
 
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Cape Town&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
