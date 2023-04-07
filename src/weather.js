34514177b102326402dee6c6653992071ebf4791
Siyabonga-sudo <siyabongamsipa00@gmail.com>
2023-04-07 18:29:49 +0200

    Added current time function

--- src/weather.js
+++ src/weather.js
@@ -1,3 +1,31 @@
+function currentDayTime(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+let dayTime = document.querySelector("#day-time");
+let date = new Date();
+dayTime.innerHTML = currentDayTime(date);
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
